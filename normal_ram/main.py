9737927d6b93e79f6c54d07898fa30b95432c1a9
clement3872 <clement.noisy78@gmail.com>
2024-05-02 06:11:02 +0200

    new branch test + open_ram function

--- normal_ram/main.py
+++ normal_ram/main.py
@@ -5,14 +5,39 @@ def open_file(filename):
         l = f.readlines()
     return l
 
-if __name__ == "__main__":
-    filename = sys.argv[1] if len(sys.argv)==2 else ""
+def open_ram():
+    # open file of ram code
+    filename = sys.argv[1] if len(sys.argv)>=2 else ""
     if os.path.exists(filename) and os.path.isfile(filename):
         content = open_file(filename)
         ram = initialise(content)
     else: 
-        #print("This is not a proper file name.")
+        #print(f"This is not a proper file name: {filename}")
+        # return 1
 
         ## This is for tests purpuses
-        content = open_file("example.ramed")
-        print(content)
+        content = open_file("ram_code/example.ramed")
+        input = []
+
+    if len(sys.argv)>2:
+        input = list(sys.argv[2:])
+    
+    for i in range(len(content)):
+        content[i] = content[i].replace("\n","").replace("\t","").replace(" ","").lower()
+
+    # first line is (rs,os) - rs = working register size - os = output size
+    reg_s,out_s = content.pop(0).split(","); reg_s = int(reg_s); out_s = int(out_s)
+
+    # second line are register names
+    reg_names = content.pop(0); reg_names = reg_names.split(",")
+    for el in reg_names: 
+        if el in ("i","is","r","rs","o","os"): # forbidden names
+            print(f"This register name is forbidden: {el}")
+
+    print(reg_s, out_s)
+    print(reg_names)
+    print(content) # content is now a list of instructions
+
+
+if __name__ == "__main__":
+    open_ram()
