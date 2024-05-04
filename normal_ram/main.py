17cab3755d5b6f4f9560cda9d44880dcaa9a2b2a
clement3872 <clement.noisy78@gmail.com>
2024-05-04 06:32:41 +0200

    update for add,sub,mult

--- normal_ram/main.py
+++ normal_ram/main.py
@@ -6,28 +6,60 @@ def open_file(filename):
         l = f.readlines()
     return l
 
-def get_args(line, instr):
-    return line[1:].replace(instr,"").replace(")","").split(",")
+def get_args(ram, line, instr):
+    l_str_args = line.replace(instr,"")[1:].replace(")","").split(",")
+    l_args = []
 
-def get_instruction(line):
+    def is_int(el):
+        return el.isdigit() or (el[0]=="-" and el[1:].isdigit())
+        
+    for arg in l_str_args:
+        if is_int(arg):
+            l_args.append(int(arg))
+            l_args.append(None)
+        elif '@' in arg:
+            tmp = arg.split("@")
+
+            var = ram.find(tmp[0])
+            if var == None:
+                assert is_int(tmp[0]), f"In line `{line}`, @ is used incorrectly"
+                var = int(tmp[0])
+
+            adr = ram.find(tmp[1])
+            if adr == None:
+                assert is_int(tmp[1]), f"In line `{line}`, @ is used incorrectly"
+                adr = int(tmp[1])
+
+            l_args.append(var)
+            l_args.append(adr)
+        else:
+            tmp = ram.find(arg)
+            assert tmp!=None, f"incorrect line: `{line}`"
+            l_args.append(tmp)
+            l_args.append(None)
+    return l_args
+
+
+def get_instruction(ram, line):
     """get instruction from a line"""
 
     # parse the instruction type
     if line[:4] in ("add(", "sub("):
         instr = line[:3]
-        l_args = get_args(line, instr)
-        #var1, var2, var3, adr1, adr2, adr3
+        var1, adr1, var2, adr2, var3, adr3 = get_args(ram, line, instr)
+        return ramer.MathOP(var1, var2, var3, adr1, adr2, adr3, instr)
     elif line[:5] == "mult(":
         instr = line[:4]
-        l_args = get_args(line, instr)
+        var1, adr1, var2, adr2, var3, adr3 = get_args(ram, line, instr)
+        return ramer.MathOP(var1, var2, var3, adr1, adr2, adr3, instr)
     elif line[:5] == "div2(":
         instr = line[:4]
         l_args = get_args(line, instr)
     elif line[:3] in ("je(","jl(","jg("):
         instr = line[:2]
         l_args = get_args(line, instr)
-    else: # it's a jump
-        instr = "jump("
+    elif line[:5] == "jump(":
+        instr = line[:4]
         l_args = get_args(line, instr)
         instr = ramed.Jump(int(l_args[0]))
 
@@ -55,25 +87,32 @@ def open_ram():
     for i in range(len(content)):
         content[i] = content[i].replace("\n","").replace("\t","").replace(" ","").lower()
 
-    # first line is (rs,os) - rs = working register size - os = output size
-    r_s,out_s = content.pop(0).split(","); r_s = int(r_s); out_s = int(out_s)
-    r_s = ramer.RegisterInt("rs",int(r_s)); out_s = ramer.RegisterInt("os",int(out_s))
-    r = ramer.RegisterArray("r", [0 for _ in range(r_s.value)])
-    o = ramer.RegisterArray("o", [0 for _ in range(out_s.value)])
+    ## first line is (rs,os) - rs = working register size - os = output size
+    ## Do we need this though?
+    # r_s,out_s = content.pop(0).split(","); r_s = int(r_s); out_s = int(out_s)
+    # r_s = ramer.RegisterInt("rs",int(r_s)); out_s = ramer.RegisterInt("os",int(out_s))
+    r = ramer.RegisterArray("r", [])
+    o = ramer.RegisterArray("o", [])
 
     # second line are register names
     reg_names = content.pop(0); reg_names = reg_names.split(",")
-    registers = [inp, inp_s, r, r_s,o,out_s]
+    # registers = [inp, inp_s, r, r_s,o,out_s]
+    registers = [inp, inp_s, r,o]
     for el in reg_names: 
         if el in ("i","is","r","rs","o","os"): # forbidden names
             print(f"This register name is forbidden: {el}")
+        else:
+            registers.append(ramer.RegisterInt(el, 0))
 
-    for el in registers:
-        print(el)
+    ram = ramer.Ram(registers)
 
     l_instr = []
-    for line in content: l_instr.append(get_instruction(line))
+    for line in content: 
+        #l_instr.append(get_instruction(ram, line))
+        ram.append_instruction(get_instruction(ram,line))
 
+    ram.run()
+    print(ram)
 
 if __name__ == "__main__":
     ram = open_ram()
--- normal_ram/ramer.py
+++ normal_ram/ramer.py
@@ -6,10 +6,14 @@ class RegisterInt:
         self.value = value
 
     def __str__(self):
-        return f"[name: {self.name}; value: {self.value}]"
+        # return f"[name: {self.name}; value: {self.value}]"
+        return f"[{self.name} = {self.value}]"
     
     def __add__(self, reg2):
         return self.value + reg2.value
+    
+    def set(self, new_value):
+        self.value = new_value
 
 class RegisterArray:
     def __init__(self, name, data=[]):
@@ -18,20 +22,76 @@ class RegisterArray:
         self.size = len(data)
 
     def __str__(self):
-        return f"[name: {self.name}; data: {self.data}; size: {self.size}]"
+        # return f"[name: {self.name}; data: {self.data}; size: {self.size}]"
+        return f"[{self.name} = {self.data}; size={self.size}]"
+    
+    def get(self, index):
+        if type(index) == int:
+            return self.data[index]
+        elif type(index) == RegisterInt:
+            return self.data[index.get()]    
+
+    def set(self, index, new_value):
+        # this should not happen
+        assert type(new_value) == int, "[RegisterArray.set] type of new_value should be an int" 
+
+        while index >= self.size:
+            self.size += 1 
+            self.data.append(0)
+
+        if type(index) == int:
+            self.data[index] = new_value
+        elif type(index) == RegisterInt:
+            self.data[index.get()] = new_value
 
 
 class MathOP:
     """Math operations add, sub, mult"""
-    def __init__(self, var1, var2, var3, adr1, adr2, adr3, op_type="add", value=0):
+    def __init__(self, var1, var2, var3, adr1=None, adr2=None, adr3=None, op_type="add"):
+        assert type(var1) in (int, RegisterInt, RegisterArray), "var1 in not a int or RegisterInt or RegisterArray"
+        assert type(var2) in (int, RegisterInt, RegisterArray), "var2 in not an int or RegisterInt or RegisterArray"
+        assert type(var3) in (RegisterInt, RegisterArray), "var3 in not a RegisterInt or RegisterArray"
+
         self.var1, self.var2, self.var3 = var1, var2, var3
         self.adr1, self.adr2, self.adr3 = adr1, adr2, adr3
 
         self.op_type = op_type
         self.category = "math"
 
+    def __str__(self):
+        if type(self.var1) == int: v1 = self.var1
+        elif type(self.var1) == RegisterInt: v1 = self.var2
+        else: v1 = self.var1.get(self.adr1)
+
+        return f"[ {self.op_type}({self.var1}@{self.adr1}, {self.var2}@{self.adr2}, {self.var3}@{self.adr3}) ]"
+
     def do(self):
-        pass
+        """Applies the instruction"""
+        if type(self.var1) == int: v1 = self.var1
+        elif type(self.var1) == RegisterInt: v1 = self.var1.value
+        else: v1 = self.var1.get(self.adr1)
+
+        if type(self.var2) == int: v2 = self.var2
+        elif type(self.var2) == RegisterInt: v2 = self.var2.value
+        else: v2 = self.var2.get(self.adr2)
+
+        if self.op_type == "add":
+            if type(self.var3)==RegisterInt:
+                self.var3.set(v1+v2)
+            else: 
+                self.var3.set(self.adr3, v1+v2)
+        elif self.op_type == "sub":
+            if type(self.var3)==RegisterInt:
+                self.var3.set(v1-v2)
+            else: 
+                self.var3.set(self.adr3, v1-v2)
+        elif self.op_type == "mult":
+            if type(self.var3)==RegisterInt:
+                self.var3.set(v1*v2)
+            else: 
+                self.var3.set(self.adr3, v1*v2)
+        return 1 # jump 1 line 
+
 
 class Div2:
     """Division by 2 (removes last bit)"""
@@ -49,15 +109,21 @@ class Div2:
             return f"div2({self.var1.name})"
 
     def do(self):
+        """Applies the instruction"""
         if type(self.var1)==RegisterArray:
             self.var1.data[self.adr1] = self.var1.data[self.adr1] >> 1 # devide by 2
         else:
             self.var1 = self.var1 >> 1 # devide by 2
+        return 1 # jump 1 line 
 
 
 class JumpLike:
     """JumpLike in (je, gl, jg)"""
-    def __init__(self, op_type="je", value=1):
+    def __init__(self, op_type, var1, var2, adr1=None, adr2=None, value=1):
+        assert type(var1) in (int, RegisterInt, RegisterArray), "var1 in not a int or RegisterInt or RegisterArray"
+        assert type(var2) in (int, RegisterInt, RegisterArray), "var2 in not an int or RegisterInt or RegisterArray"
+
+        assert type(value) == int, "A jump value should be a int"
         assert value != 0, "A jump should jump a line, not 0"
 
         self.value = value
@@ -65,10 +131,11 @@ class JumpLike:
         self.category = "jump"
 
     def __str__(self):
-        return f"{self.op_type}({self.value})"
+        return f"{self.op_type}({self.var1}@{self.adr1}, {self.var2}@{self.adr2}, {self.value})"
 
     def do(self):
-        pass
+        """Applies the instruction"""
+        return self.value
 
 
 class Jump:
@@ -84,6 +151,7 @@ class Jump:
         return f"Jump({self.value})"
 
     def do(self):
+        """Applies the instruction"""
         return self.value
 
 
@@ -91,9 +159,67 @@ class Ram:
     def __init__(self, l_reg, l_instr=[]):
         self.registers = l_reg.copy()
         self.instructions = l_instr.copy()
+        self.cursor = 0
 
-    def add_instruction(self, instruction):
-        pass
+    def __str__(self):
+        s = "RAM:\n-Registers:\n"
+        if len(self.registers) == 0: s += "(None)\n"
+
+        # display RAM's registers
+        for el in self.registers:
+            s = s + '\t' + el.__str__() + "\n"
+        s += "\n-Instructions:\n"
+
+        # display RAM's instructions
+        if len(self.instructions) == 0: s += "(None)\n"
+        for el in self.instructions:
+            s = s + '\t' + el.__str__() + "\n"
+
+        s += f"\nCursor position: {self.cursor}"
+        return s
+
+    def find(self, name):
+        for reg in self.registers:
+            if reg.name == name: return reg
+        return None
+    
+    def print_reg_status(self):
+        if len(self.registers)>0:
+            print("Registers status:")
+            for reg in self.registers:
+                print(f"\t{reg}")
+
+    def print_next(self):
+        """Prints the next step"""
+        if self.cursor < len(self.instructions):
+            print(f"Next instruction:\n\t{self.instructions[self.cursor]}")
+        
+    def append_instruction(self, instruction):
+        """Does what is says"""
+        self.instructions.append(instruction)
+
+    def next(self):
+        """Does the next step"""
+        self.cursor += self.instructions[self.cursor].do()
+
+    def run(self, verbose=False):
+        print("\nRAMing...")
+        if verbose:
+            self.print_reg_status()
+            self.print_next()
+            print(f"Cursor: {self.cursor}")
+            input("\nPress Enter to continue")
+        while self.cursor < len(self.instructions):
+            self.next()
+            if verbose:
+                self.print_reg_status()
+                self.print_next()
+                print(f"Cursor: {self.cursor}")
+                input("\nPress Enter to continue")
+        if not verbose: print("(...)")
+        out = self.find("o")
+        if out != None: print(f"Output register: {out}")
+        print(f"You got RAMed in {self.cursor} steps!\n")
 
 
 if __name__ == "__main__":
